5b0c98fb32db2d2bf3bff010b74730fb43e5ba7d
otari nozadze <otikonozadze@gmail.com>
2025-07-09 16:59:37 +0300

    hello world from conflict

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("this is conflict");
+        System.out.println("this is conflict from confclit");
     }
 }
